318b4cdacae7ebdc93bcc1546e7469606292852e
Cameron <cameronkuy@gmail.com>
2023-12-23 12:09:36 -0800

    Initial Commit

---
+++ Music Player/Music_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_PlayerApp.swift
+//  Music Player
+//
+//  Created by Cameron Kuykendall on 12/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Music_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
